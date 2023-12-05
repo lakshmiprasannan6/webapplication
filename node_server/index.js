aea129f0cfb2df408eadb159e3c8213f0bb931c2
lakshmiprasannan6 <153021038+lakshmiprasannan6@users.noreply.github.com>
2023-12-05 14:59:43 -0500

    Add files via upload

---
+++ node_server/index.js
@@ -0,0 +1,71 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+                    (err, content) => {
+                                    if (err) throw err;
+                                    res.setHeader("Access-Control-Allow-Origin", "*")
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    res.end(content);
+                        }
+              );
+     }
+     else if (req.url === '/style.css') {
+        fs.readFile(path.join(__dirname, 'public', 'style.css'),
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    res.setHeader("Access-Control-Allow-Origin", "*")
+                                    res.writeHead(200, { 'Content-Type': 'text/css' });
+                                    res.end(content);
+                        }
+              );
+     }
+     else if (req.url.startsWith('/Assets/')) {
+    const assetPath = path.join(__dirname, req.url);
+    fs.readFile(assetPath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+        return;
+      }
+      const contentType = path.extname(assetPath) === '.png' ? 'image/png' : 'image/jpeg';
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    });
+  }
+    else if (req.url === '/about') {
+        fs.readFile(
+            path.join(__dirname, 'public', 'about.html'),
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    res.setHeader("Access-Control-Allow-Origin", "*")
+                                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                                    res.end(content);
+                        }
+              );
+     }
+    else if (req.url==='/api')
+    {
+        fs.readFile(
+            path.join(__dirname, 'public', 'db.json'),'utf-8',
+                    (err, content) => {
+                                    
+                                    if (err) throw err;
+                                    res.setHeader("Access-Control-Allow-Origin", "*")
+                                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                                    res.end(content);
+                        }
+              );
+    }
+    else{
+        res.end("<h1> 404 nothing is here</h1>");
+    }
+});
+
+const PORT= process.env.PORT || 5656;
+
+server.listen(PORT,()=> console.log(`Great our server is running on port ${PORT} `));
